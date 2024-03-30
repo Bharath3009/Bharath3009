f2b5b88016b199df7ff0de1dd710a908a56dc99d
Bharath3009 <165463339+Bharath3009@users.noreply.github.com>
2024-03-30 14:41:25 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import re
+import sys
+from openai.error import __main__
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    sys.exit(main())
+
+
+
+
+
+
